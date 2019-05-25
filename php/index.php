af7bcf1c1f79d035ed6545606a8d75cb16a17b91
Jason <kevinblank72@gmail.com>
2019-05-25 17:37:13 +0200

    sql code toegevoegd aan index.php

--- php/index.php
+++ php/index.php
@@ -6,7 +6,6 @@
   <meta charset="utf-8">
   <title>Register you're artist</title>
 </head>
-<?php include 'connect.php' ?>
 <body>
   <main>
     <h1>Register the artist below</h1>
@@ -17,17 +16,35 @@
       <input type="text" name="title" placeholder="type here">
       <button type="submit" name="submit">Submit</button>
     </form>
-
-<?php
-  //selecteerd van tabel songs
-  $sql = 'SELECT * FROM `songs`';
-  $results = $conn->query($sql);
-
-  //pakt een record en maakt er een associatief array van
-  while ($row = $results->fetch_assoc()){
-     echo print_r($row)."<br>";   //hier moet ik een table aan koppelen
-  }
-?>
   </main>
+  <?php
+   include_once 'connect.php' ;
+
+    //selecteerd van tabel songs
+    $sql = 'SELECT * FROM `songs`';
+    $results = $conn->query($sql);
+
+    //pakt een record en maakt er een associatief array van
+    if (mysqli_num_rows($results) >0) {
+      while ($row = $results->fetch_assoc()){
+        $data[] = $row;
+        echo print_r($row)."<br>";   //hier moet ik een table aan koppelen
+     }
+   }//code hieronder verwerken in code hierboven!
+  ?>
+  <table>
+    <tr>
+      <td>Artist</td>
+      <td>Title</td>
+      <td>ID</td>
+    </tr>
+    <?php foreach ($results as $row) : ?>
+    <tr>
+      <td><?php echo $row[0]; ?></td>
+      <td><?php echo $row[1]; ?></td>
+      <td><?php echo $row[2]; ?></td>
+    </tr>
+  <?php endforeach; ?>
+</table>
 </body>
 </html>
