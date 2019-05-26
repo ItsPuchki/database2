b21c38b920e08fd797d287664b5c3a8481a44252
Jason <kevinblank72@gmail.com>
2019-05-26 20:20:55 +0200

    styling aan website toegevoegd en aanpassingen gemaakt aan index.php

---
+++ oefening1/php/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <link rel="stylesheet" href="../css/master.css">
+  <meta charset="utf-8">
+  <title>Register you're artist</title>
+</head>
+
+<body>
+  <h1>Register the artist below</h1>
+    <form class="" action="create.php" method="post" autocomplete="off">
+      <p>Artist</p>
+      <input type="text" name="artist" placeholder="type here">
+      <p>Title</p>
+      <input type="text" name="title" placeholder="type here">
+      <button type="submit" name="submit">Submit</button>
+    </form>
+
+    <?php
+    include_once 'connect.php' ;
+    //selecteerd van tabel songs
+    $sql = 'SELECT * FROM `songs`';
+    $results = $conn->query($sql);
+    //pakt een record en maakt er een associatief array van(op line 26 & 27)
+    if (mysqli_num_rows($results) >0) {
+      while ($row = $results->fetch_assoc()){
+        //$data[] = $row;
+        echo "<table>"$row['<td>''artist''<td>']." ".$row['title']." ".$row['id']."<br>" "</table>";
+      }
+      //shows results
+      echo 'Total of results is: ' . $results->num_rows;
+      //shows updates
+      echo "<br>".'Total of rows updated: ' . $conn->affected_rows;
+    }
+
+    //deze code toepassen zodat de echo de tables gebruikt
+    /*  if ($result->num_rows > 0) {
+            echo "<div class='tablehoofd'><h2><tr><td>num</td>&emsp;&emsp;<td>naam</td>&emsp;&emsp;<td>achter_naam</td>
+            &emsp;&emsp;<td>geboortedatum</td>&emsp;&emsp;&emsp;&emsp;<td>leeftijd</td></h2></div>"; /*echo de top rij met opties
+
+            while($row = $result->fetch_assoc()) {
+              echo "<div class='table'>".$row['num']."<div class='locatie'>".$row['naam']."</div><div class='locatie2'>".$row['achter']."</div>
+              <div class='locatie3'>".$row['geboortedatum']."</div><div class='locatie4'>".$row['age']."</div></div>";
+            } */
+
+    ?>
+</body>
+</html>
